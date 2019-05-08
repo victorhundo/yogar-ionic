1da41d65d90692dd4f0cf79c157ec183858a9e4f
victorhundo <victorhundo@gmail.com>
2019-05-08 14:25:38 -0300

    Adicionado logout

--- src/app/pages/tabsController/tab3/tab3.module.ts
+++ src/app/pages/tabsController/tab3/tab3.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }])
+    RouterModule.forChild([{ path: '', component: Tab3Page }]),
+    MatFormFieldModule,
+    MatButtonModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/pages/tabsController/tab3/tab3.page.ts
+++ src/app/pages/tabsController/tab3/tab3.page.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+
+  ngOnInit() { }
+
+  constructor(private router: Router) { }
+
+
+  primeiroNome = JSON.parse(localStorage.getItem('login')).user.primeiroNome;
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/logar'])
+  }
+}
